a8f12a043aafe37e09f17367e566ed35a465315f
BholeyKaBhakt <bholekabhakt@proton.me>
2024-07-25 00:44:47 +0530

    build: Use ReVanced Patches plugin

---
+++ patches/build.gradle.kts
@@ -0,0 +1,13 @@
+group = "io.github.bholeykabhakt"
+
+patches {
+    about {
+        name = "Xtra ReVanced Patches"
+        description = "Xtra Patches template for ReVanced."
+        source = "git@github.com:bholeykabhakt/revanced-patches-xtra.git"
+        author = "BholeyKaBhakt"
+        contact = "bholeykabhakt@proton.me"
+        website = "https://bholeykabhakt.github.io"
+        license = "GNU General Public License v3.0"
+    }
+}
